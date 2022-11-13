a426a95a6eec89b2f9366d0aa62fd19991d4596d
taman-tech <108014156+taman-tech@users.noreply.github.com>
2022-11-13 20:53:19 +0300

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -10,10 +10,10 @@ logging.getLogger("urllib3").setLevel(logging.WARNING)
 
 
 class ENV_VARS(object):
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME")
+    API_ID = int(os.environ.get("11496461"))
+    API_HASH = os.environ.get("ab52f38fc71b58e707bf9e9aef4c9963")
+    BOT_TOKEN = os.environ.get("5632952116:AAHeZw2x4FeizqyPJaAwY-SiMemoCjuLTXU")
+    BOT_USERNAME = os.environ.get("snapchatbytamanbot")
     #AUTH_USER = int(os.environ.get("AUTH_USER", 5071059420))
 
 
